10b968cf546a0be98044312b25060260d7a81a7e
David Lee <1313dhl@gmail.com>
2019-10-15 15:04:51 -0500

    start react proj

---
+++ conversational-interface/src/LandingPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './LandingPage.css';
+
+class LandingPage extends Component {
+
+
+
+
+  
+
+
+
+
+    render() {
+      return (
+            <p class='button2' id ="button">
+              This is the landing page component
+            </p>
+        
+      );
+    }
+
+  }
+  
+  export default LandingPage;
+  
