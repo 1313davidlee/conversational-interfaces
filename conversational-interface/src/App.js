9040ff753c195fb5225bc33bbf79e2e9c4a612b4
Carrie Douglass <carriecdouglass@me.com>
2019-10-17 16:12:49 -0500

    nothing

--- conversational-interface/src/App.js
+++ conversational-interface/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import LandingPage from './LandingPage.js'
+import InteractionsPage from './InteractionsPage.js'
 import './App.css';
 
 class App extends Component {
