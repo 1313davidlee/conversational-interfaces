d14b9d12730ee0bb40edc365d9dd3c5ea1c603e3
Carrie Douglass <carriecdouglass@me.com>
2019-10-24 14:16:40 -0500

    changed summary panel

--- conversational-interface/src/ArticleClip.js
+++ conversational-interface/src/ArticleClip.js
@@ -5,7 +5,12 @@ class ArticleClip extends Component {
     render() {
         return (
             <div className='APtext'>
-                
+                <div className='APtextHeadlineContent'>
+                    Biden Calls for Trump Impeachment for First Time
+                    <div className='APtextShortSummaryContent'>
+                        It was the first time the former vice president expressed support for full impeachment. He had previously backed a congressional inquiry.
+                    </div>
+                </div>
             </div>
         )
     }
