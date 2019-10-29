edfbd1b7edd39e6638c497020e0d67cd6e06fa66
Carrie Douglass <carriecdouglass@me.com>
2019-10-29 16:22:10 -0500

    working on aesthetics of interactions page

--- conversational-interface/src/QAPanel.js
+++ conversational-interface/src/QAPanel.js
@@ -13,20 +13,30 @@ class QAPanel extends Component {
     }
     render() {
       return (
-       
+        <div>
             <div className='QAPanel'>
                 <div className='IDquestionBar'>
 
-                    Question: {this.state.question}
+                    {this.state.question}
                 </div>
                 <div className='IDanswerBar'>
                     <div className='IDanswerQuote'>
-                    Answer: {this.state.answer}</div>
-                    <div className='IDanswerSource'>
-                    Source: {this.state.source}</div>
+                    {this.state.answer}</div>
                 </div>
             </div>
-        
+            <div className='divider'>
+            </div>
+            <div className='noAnswer'>
+                <div className='IDquestionBar'>
+                    What is Elizabeth Warren's favorite color?
+                </div>
+                <div className='IDanswerBar'>
+                    <div className='IDanswerQuote'>
+                        Unfortunately, we could not find the answer to your question. You might want to check out the links in Further Reading!
+                    </div>
+                </div>
+            </div>
+        </div> 
       );
     }
   }
