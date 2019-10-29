3991a5e8711e8a434b5be99a71f4ac017e016e54
Carrie Douglass <carriecdouglass@me.com>
2019-10-28 21:05:06 -0500

    changed summary panel

--- conversational-interface/src/SummaryPanel.js
+++ conversational-interface/src/SummaryPanel.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import './SummaryPanel.css';
-import ArticleClip from './ArticleClip.js'
 
 
 class SummaryPanel extends Component {
     render() {
         return (
             <div className='SPbackground'>
-                <div className='SPheaderBar'>
-                    <div className="SPheaderBarContent">
-                        Summary
+                <div className='SPheader'>
+                    Links from the Article
+                </div>
+                <div className='wrapper'>
+                    <img class="image" src="http://placekitten.com/200/200"></img>
+                    <div class="headline">
+                        Biden Calls for Trump Impeachment for First Time
                     </div>
-                </div>
-                <div className='SParticleClip'>
-                    <ArticleClip></ArticleClip>
-                </div>
-                    
-                <div className='SPsummary'></div>
+                    <div class="description">
+                        It was the first time the former vice president expressed support for full impeachment. He had previously backed a congressional inquiry.
+                    </div> 
+                </div>   
+                <div className='linkBox'></div>
             </div>
         )
     };
