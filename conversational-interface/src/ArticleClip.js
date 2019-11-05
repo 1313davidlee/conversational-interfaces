adbee3f4b8b45dd8882948665e7e8eea6c466d08
David Lee <1313dhl@gmail.com>
2019-11-05 16:14:58 -0600

    force commit

---
+++ conversational-interface/src/ArticleClip.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './ArticleClip.css';
+
+class ArticleClip extends Component {
+    render() {
+        return (
+            <div className='APtext'>
+                <div className='APtextHeadlineContent'>
+                    Biden Calls for Trump Impeachment for First Time
+                    <div className='APtextShortSummaryContent'>
+                        It was the first time the former vice president expressed support for full impeachment. He had previously backed a congressional inquiry.
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ArticleClip;
