8242045ee39ffe5edbe92ef2105eb3325b83df38
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-11-26 15:14:32 -0600

    standard commits

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import request, jsonify
+from urllib.parse import urlparse
 from app import app
 
 @app.route('/')
 
 @app.route('/myurl')
 def myurl():
+    url = urlparse('')
     param = request.args.get('foo', 'you didnt send anything')
     return jsonify({
         'input':param,
